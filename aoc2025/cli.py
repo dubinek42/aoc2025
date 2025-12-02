3946df1fbfdd04d8fed64d28e1d3fac9380c1ff4
Tomas Haas <tomas.haas@rossum.ai>
2025-12-02 09:56:10 +0100

    feat: time performance

--- aoc2025/cli.py
+++ aoc2025/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import importlib
+import time
 
 from aoc2025.utils.io import load_input
 
@@ -16,9 +17,11 @@ def main() -> None:
 
     input_text = load_input(day=args.day, sample=args.sample)
 
+    start_time = time.perf_counter()
     if args.part in (None, 1):
         result1 = module.solve_part1(input_text)
-        print(f"Day {args.day:02d}, Part 1: {result1}")
+        print(f"Day {args.day:02d}, Part 1: {result1}, Time: {(time.perf_counter() - start_time):.4f} s")
+        start_time = time.perf_counter()
     if args.part in (None, 2):
         result2 = module.solve_part2(input_text)
-        print(f"Day {args.day:02d}, Part 2: {result2}")
+        print(f"Day {args.day:02d}, Part 2: {result2}, Time: {(time.perf_counter() - start_time):.4f} s")
